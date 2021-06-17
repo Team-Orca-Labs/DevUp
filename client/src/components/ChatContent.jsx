eb1ee322e8e11e526cf79490e26da4efde50b054
Raubern Totanes <rstotanes@g.ucla.edu>
2021-06-17 04:23:26 -0400

    add chat component. edits to styling. empty tests

--- client/src/components/ChatContent.jsx
+++ client/src/components/ChatContent.jsx
@@ -3,6 +3,7 @@ import { connect, useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import * as actions from '../actions/actions';
 import { Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom';
+import { API_URL } from "../env";
 
 function mapStateToProps(state) {
   return {
@@ -29,42 +30,62 @@ function ChatContent(props) {
   }, [id]);
 
   let matchesNames = [];
-  let sampleNames = 
-    [
-      <div id='chatNames'>victor</div>, 
-      <div id='chatNames'>jeff</div>,
-      <div id='chatNames'>raubern</div>,
-      <div id='chatNames'>matt</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-      <div id='chatNames'>davette</div>,
-    ]
+  let matchesChats = [];
+  let sampleNames = ['victor', 'jeff', 'raubern', 'matt', 'davette','victor', 'jeff', 'raubern', 'matt', 'davette','victor', 'jeff', 'raubern', 'matt', 'davette','victor', 'jeff', 'raubern', 'matt', 'davette'];
+
   let { path, url } = useRouteMatch();
 
-  for (let i = 0; i < props.matches; i++) {
+  for (let i = 0; i < sampleNames.length; i++) {
     matchesNames.push(
-      <div id='chatName'>
-        <Link to={`${url}/:${props.matches[i].username}`} />
-      </div>
+      <Link 
+        to={`${url}/:${sampleNames[i]}`} 
+        id='chatlink'
+        key={i}
+      >
+      {sampleNames[i]}
+      </Link>
     );
+    matchesChats.push(
+      <Route
+        path={`${url}/:${sampleNames[i]}`}
+        key={i}
+      >
+      </Route>
+    )
   }
+
+  // for (let i = 0; i < props.matches; i++) {
+  //   matchesNames.push(
+  //     <div id='chatName'>
+  //       <Link to={`${url}/:${props.matches[i].username}`} />
+  //     </div>
+  //   );
+  // }
 
   function sendMessage() {
     // something something websockets
+    // whats the endpath for database updates?
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(actions.updateMessages(data.messages));
+        })
+        .catch((err) => console.log(err));
   }
+
+  // useEffect(() => {
+  //   if (id) {
+  //     const url = API_URL + `/matches?userId=${id}`;
+  //     fetch(url)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         dispatch(actions.updateMatches(data.matches));
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // }, []);
+
+  // }
 
   return (
     <div id='chatContent'>
@@ -73,29 +94,28 @@ function ChatContent(props) {
       </center>
         
       {/* <nav>{matchesNames}</nav> */}
-      <nav>{sampleNames}</nav>
+      <nav>{matchesNames}</nav>
       {/* ROUTER???? to switch  */}
       <main id='singlechat'>
+
         <Switch>
           <Route exact path={path}>
             <h2>No chat selected</h2>
           </Route>
-          <Route path={path}>
-          {/* <Route path={`${path}/${props.matches[i].username}`}> */}
-            {/* {DISPLAY CHAT MESSAGES HERE} */}
-            <div>
-              {/* {something something websockets} */}
-            </div>
-            <input
-              id="nameInput"
-              type="text"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}>
-            </input>
-            <button onClick={sendMessage}>Send</button>
-          </Route>
+          {matchesChats}
         </Switch>
-      </main>  
+        
+        <div id='inputsend'>
+          <input
+            id="messageInput"
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}>
+          </input>
+          <button onClick={sendMessage}>Send</button>
+        </div>
+      
+      </main> 
     </div>
   )
 }
